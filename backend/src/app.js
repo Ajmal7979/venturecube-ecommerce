00890f6b22d8a5f9ac61560a94c48d4cf4b0078c
Ajmal7979 <152248654+Ajmal7979@users.noreply.github.com>
2026-02-01 15:42:48 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,16 +31,12 @@ app.use(
 );
 
 
-
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/addresses", addressRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/orders", orderRoutes);
 app.use("/api/seller", sellerRoutes);
 app.use("/api/chat", chatRoutes);
 
